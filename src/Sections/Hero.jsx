2d8b62430296c5b014ba4f8101c42056d92d1865
Monjur Hossen <14monjurhossen@gmail.com>
2024-02-07 02:55:49 +0600

    Hero section 2

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
 import SphereAnimation from "../Components/sphereAnimation"
 
 /* eslint-disable no-mixed-spaces-and-tabs */
 const Hero = () => {
   return (
-    <div className="w-full h-screen bg-black">
+    <div className="w-full bg-black">
       <header className="contain">
-        <nav className="flex justify-between items-center py-5">
-            <img src="logo.svg" alt="" className="w-[130px] lg:w-36"/>
+        <nav className="flex justify-between items-center py-4 md:py-5">
+            <img src="download1.svg" alt="" className="w-[130px] lg:w-36"/>
             <ul className="text-white hidden xmd:flex gap-6 text-lg font-semibold">
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
@@ -15,9 +16,9 @@ const Hero = () => {
                 <li><a href="">Contact</a></li>
             </ul>
         </nav>
-        <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col lg:flex-row gap-10 lg:gap-0">
             <div className="basis-1/2">
-                <div className=" border-white -space-y-[6rem] lg:-space-y-[5rem]">
+                <div className=" border-white -space-y-[6.18rem] lg:-space-y-[5rem] -mt-7 lg:-mt-3">
                     <svg className="svg1">
 		                <text x="0px" y="50%" dy=".35em" className="text1">Hi, I am</text>
 	                </svg>
@@ -31,10 +32,15 @@ const Hero = () => {
 		                <text x="0px" y="50%" dy=".35em" className="text4">Developer</text>
 	                </svg>
                 </div>
+                <div className=" border-white text-lowWhite pr-10">
+                  <div className="-mt-9 lg:-mt-5 text-">
+                    <p className="hidden md:inline hero_p">Passionate Frontend Developer</p><p className="inline hero_p"> specializing in React.js.</p><p className="inline hero_p"> I transform ideas into interactive and visually stunning web experiences.</p><p className="hidden md:inline hero_p"> Your Vision, My Code - Let's Build Something Extraordinary Together</p>
+                  </div>
+                </div>
             </div>
-            <div className="basis-1/2">
+            <div className="basis-1/2 mb-10">
                 <div className="w-full lg:w-[30rem] mx-auto">
-                <SphereAnimation/>
+                    <SphereAnimation/>
                 </div>
             </div>
         </div>
